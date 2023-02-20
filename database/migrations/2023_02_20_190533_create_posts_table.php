5e8e529fcbe291e8d3c12fa8e36a3eea3eba8b27
6-ON <aminhatim6@gmail.com>
2023-02-20 21:56:49 +0100

    done with posts migration

--- database/migrations/2023_02_20_190533_create_posts_table.php
+++ database/migrations/2023_02_20_190533_create_posts_table.php
@@ -11,8 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create  ('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('slug')->unique();
+            $table->string('image');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('attachment');
             $table->timestamps();
         });
     }
