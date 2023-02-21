0c879276cdb8f0a95e9acda7df9048a9e154512b
6-ON <aminhatim6@gmail.com>
2023-02-21 15:10:33 +0100

    ReactionFactory done

--- database/factories/ReactionFactory.php
+++ database/factories/ReactionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,9 @@ class ReactionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id'=>User::factory(),
+            'post_id'=>Post::factory(),
+            'emoji'=>fake()->emoji()
         ];
     }
 }
