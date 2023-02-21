6460146437f49039a087fd9e91e3c337d308de66
6-ON <aminhatim6@gmail.com>
2023-02-21 15:18:13 +0100

    change description string to text

--- database/migrations/2023_02_20_190533_create_posts_table.php
+++ database/migrations/2023_02_20_190533_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create  ('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('slug')->unique();
             $table->string('image');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
