cd0c1db17bade07e75c6ff54d398f5eebf611ad8
6-ON <aminhatim6@gmail.com>
2023-02-22 21:16:21 +0100

    finish crud

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,67 +1,86 @@
-<?php
+<?php /** @noinspection ALL */
+
+/** @noinspection PhpUndefinedFieldInspection */
 
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): Response
-    {
-        //
+        \request([]);
+        return Post::filter()->get();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        //
+        $isValid=  $request->validate([
+            'title'=>'required|max:50',
+            'description'=>'required|max:500',
+            'category_id'=>'required',
+            'image'=>'required|url',
+            'attachment'=>'required|url',
+        ]);
+        if($isValid){
+            $post = Post::create([
+                'title'=>$request->title,
+                'user_id'=> $request->user()->id,
+                'description'=>$request->description,
+                'category_id'=>$request->category_id,
+                'image'=>$request->image,
+                'attachment'=>$request->attachment,
+            ]);
+            return $post;
+        }
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post): Response
+    public function show(Post $post): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post): Response
-    {
-        //
+        return \response()->json($post);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post): RedirectResponse
+    public function update(Request $request, Post $post): Response
     {
-        //
+        $isValid=  $request->validate([
+            'title'=>'required|max:50',
+            'description'=>'required|max:500',
+            'category_id'=>'required',
+            'image'=>'required|url',
+            'attachment'=>'required|url',
+        ]);
+        if($isValid){
+            $post->fill($request->all());
+            $post->save();
+            return $post;
+        }
+        return \response();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post): RedirectResponse
+    public function destroy(Post $post): Response
     {
-        //
+        $post->delete();
+        return \response()->noContent();
     }
 }
