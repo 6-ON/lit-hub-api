0895df67c7aaf3358c27c281131d04e2b051dd6f
6-ON <aminhatim6@gmail.com>
2023-02-22 20:19:21 +0100

    add eloquent Relation and Sluggable trait

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    protected $guarded =['id'];
+    protected $with = ['user:id,username', 'category:id,label,slug'];
+
+    public function scopeFilter($filters)
+    {
+
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return ['slug' => ['source' => 'title']];
+    }
 }
