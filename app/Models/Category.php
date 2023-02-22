52aa6e90cb2a95aa543287c35221b7c18408acbc
6-ON <aminhatim6@gmail.com>
2023-02-22 20:17:52 +0100

    add as Sluggable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory,Sluggable;
+
+    public function sluggable(): array
+    {
+        return ['slug' => ['source' => 'title']];
+    }
 }
