6e7ffd5f4eb70037a04ce8007ca764bdfef80bee
6-ON <aminhatim6@gmail.com>
2023-02-23 21:35:28 +0100

    add relations: owner,members,messages

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Group extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    protected $guarded = ['id'];
+    protected $hidden = ['pivot'];
+
+    public function sluggable(): array
+    {
+        return ['slug' => ['source' => 'title']];
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+    public function members()
+    {
+        return $this->belongsToMany(User::class, Membership::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
