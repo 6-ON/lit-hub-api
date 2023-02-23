da69ca2ec88b37426c5b91d1dd08bfde96e7f559
6-ON <aminhatim6@gmail.com>
2023-02-23 21:33:56 +0100

    add user relation

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $with=['user:id,username,image'];
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
