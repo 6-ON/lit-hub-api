456f831d2998caef7d6015e9ad55080b43a838c6
6-ON <aminhatim6@gmail.com>
2023-02-23 21:36:22 +0100

    add relation owner

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
