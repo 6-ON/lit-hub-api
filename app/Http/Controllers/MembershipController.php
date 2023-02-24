14b965c98e7bd9660b9202059419703d37f68b14
6-ON <aminhatim6@gmail.com>
2023-02-24 15:04:42 +0100

    finish MembershipController.php

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -2,66 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use App\Models\Membership;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class MembershipController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): Response
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): Response
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
+     * @throws ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Group $group, Request $request)
     {
-        //
+        //check if joined
+
+        $joined = $group->memberships()->where('memberships.user_id',$request->user()->id)->exists();
+
+        if ($joined) {
+            return \response()->json(['message' => 'already Joined !'], 422);
+        }
+
+        $membership = new Membership();
+        $membership->fill([
+            'group_id' => $group->id,
+            'user_id' => $request->user()->id,
+        ]);
+        $membership->save();
+        return \response()->noContent();
+
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Membership $membership): Response
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Membership $membership): Response
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Membership $membership): RedirectResponse
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
+     * @throws ValidationException
      */
-    public function destroy(Membership $membership): RedirectResponse
+    public function destroy(Group $group)
     {
-        //
+        $joined = $group->memberships()->where('memberships.user_id',\request()->user()->id)->first() ?? false;
+        abort_if(!$joined, 422);
+        $joined->delete();
+        return \response()->noContent();
+
     }
 }
