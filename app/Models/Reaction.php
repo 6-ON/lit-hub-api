f32a9e0571941e30e25878762906113f74e648af
6-ON <aminhatim6@gmail.com>
2023-02-24 16:28:34 +0100

    finish reaction model

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Reaction extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $with = ['user:id,username,image'];
+
+    protected $hidden =[
+      'post_id','user_id','created_at','updated_at'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
