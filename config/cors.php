1e4c320bca910599e4a6313ced74683c273eb88d
6-ON <aminhatim6@gmail.com>
2023-02-28 21:31:08 +0100

    remote cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'),'http://192.168.9.50:3000'],
 
     'allowed_origins_patterns' => [],
 
