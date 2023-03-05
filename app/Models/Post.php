b5a6072fbbbc36c5ac9f0b9681feaef1213ed961
6-ON <aminhatim6@gmail.com>
2023-03-06 00:59:31 +0100

    add post user_reaction

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,17 +11,17 @@ class Post extends Model
 {
     use HasFactory, Sluggable;
 
-    protected $guarded =['id'];
+    protected $guarded = ['id'];
+    protected $appends = ['user_reaction'];
     protected $with = ['owner:id,username,image', 'category:id,label,slug'];
-    protected $withCount = ['reactions','comments'];
+    protected $withCount = ['reactions', 'comments'];
     public function scopeFilter($filters)
     {
-
     }
 
     public function owner()
     {
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function category()
@@ -41,5 +42,17 @@ class Post extends Model
     public function sluggable(): array
     {
         return ['slug' => ['source' => 'title']];
+    }
+
+    protected function userReaction(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                if (request()->user() ?? false) {
+                    return $this->reactions()->where('user_id', request()->user()->id)->first();
+                }
+                return null;
+            }
+        );
     }
 }
